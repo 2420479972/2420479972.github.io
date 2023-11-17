2b2f5041efee93db0ad9f83c389940558247d5b4
2420479972 <65066122+2420479972@users.noreply.github.com>
2023-11-17 13:43:00 +0800

    1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,11 +3,11 @@
 </template>
 <script lang="ts" setup>
 import Layout from '@components/Layout/index.vue'
-import { userModel } from '../../components/Model'
-const { showModel } = userModel()
-showModel((sure: boolean, hidden) => {
-  console.log(sure)
-  hidden()
-})
+// import { userModel } from '../../components/Model'
+// const { showModel } = userModel()
+// showModel((sure: boolean, hidden) => {
+//   console.log(sure)
+//   hidden()
+// })
 </script>
 <style lang="scss" scoped></style>
